1ebf266334f8f0e2a3daa780e48f8b232de14c49
desco10 <daniel1041esk.arb@gmail.com>
2026-02-12 22:10:22 -0500

    scrroll

--- script.js
+++ script.js
@@ -196,3 +196,19 @@ document.getElementById("modal").addEventListener("click", function(e) {
     cerrarModal();
   }
 });
+
+
+
+
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
